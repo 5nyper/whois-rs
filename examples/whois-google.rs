33f80b4da951e4525f8facfd6b4f1237648ca3a4
Jesus Hernandez <jesushdez@protonmail.com>
2021-07-26 17:54:28 +0200

    Rename example of whois google.com from test to whois-google

--- examples/whois-google.rs
+++ examples/whois-google.rs
@@ -9,9 +9,10 @@ fn main() {
     let foo = &Json::from_str(&data.unwrap()).unwrap();
     let object = foo.as_object().unwrap();
     for (key, value) in object {
-        println!("{}: {}", key, match *value {
+        let value = match value {
             Json::String(ref v) => format!("{}", v),
-            _ => break
-        });
+            _ => break,
+        };
+        println!("{}: {}", key, value);
     }
 }
